b4a19b44e75ce367fb7a27bdea463b3a4905c1c4
cprajwowski <c.prajwowski.631@studms.ug.edu.pl>
2024-06-14 20:05:49 +0200

    conf mongo

--- zad10/zad2v2/mikroserwis_b/mikroserwis_b.js
+++ zad10/zad2v2/mikroserwis_b/mikroserwis_b.js
@@ -4,7 +4,7 @@ const app = express()
 
 app.get('/', (req,res) => { 
 	console.log('Dostalem tez')
-	res.send('Mikro b do a')
+	res.send('Mikro b do aaa')
 })
 
 app.listen(5000, () => { console.log('Server is running on port 5000')})
--- zad10/zad2v2/skrypt.sh
+++ zad10/zad2v2/skrypt.sh
@@ -1,2 +1,10 @@
+# docker build -t cprajwowski/lab10-zad2-mikroserwis_b ./mikroserwis_b/.
+# docker build -t cprajwowski/lab10-zad2-mikroserwis_a ./mikroserwis_a/.
+# docker push cprajwowski/lab10-zad2-mikroserwis_a
+# docker push cprajwowski/lab10-zad2-mikroserwis_b
+
 kubectl apply -f my-mikroserwis_a_deployment.yaml
 kubectl apply -f my-mikroserwis_b_deployment.yaml
+kubectl apply -f my-mongo-pv.yaml
+kubectl apply -f my-mongo-pvc.yaml
+kubectl apply -f my-mongo-pod.yaml
